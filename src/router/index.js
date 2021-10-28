15d73e9c39a87fc974628b59bb20a8c3663185ce
Nelo Puchades <correo@nelo.dev>
2021-10-28 23:25:31 +0200

    Playing around with dynamic routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
+import EventDetails from '../views/EventDetails.vue'
 import About from '../views/About.vue'
 
 const routes = [
@@ -12,6 +13,12 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About,
+  },
+  {
+    path: '/event/:id"',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails,
   },
 ]
 
